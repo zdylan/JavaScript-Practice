52c2cc940c7948949ab709d3cd35716ddcb00961
dylan_zhang_ <dylan.zhang111@gmail.com>
2017-09-22 13:25:18 +0800

    JS变量
    Signed-off-by: zhang.gai <gai.zhang@nx-engine.com>

---
+++ src/basics/variable.js
@@ -0,0 +1,23 @@
+/**
+ * Created by dylan on 2017/9/22.
+ */
+
+
+/*
+ *  var 声明变量， 声明后，该变量为 undefined.
+ * */
+
+var carname; // undefined
+
+console.log('carname--> ' + carname);
+
+/*
+ *   对变量赋值有两种方式。
+ * */
+
+var method1;
+method1 = "Volvo";
+
+var method2 = "Volvo";
+
+
