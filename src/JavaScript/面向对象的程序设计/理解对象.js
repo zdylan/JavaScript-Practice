fdab1e919c1c423cf72b49e411c0164327955656
dylan_zhang_ <dylan.zhang111@gmail.com>
2017-09-29 00:36:26 +0800

    部分面向对象的程序设计

---
+++ src/JavaScript/面向对象的程序设计/理解对象.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zhanggai on 17/9/29.
+ */
+
+/*
+*   利用首选的对象字面量创建对象
+* */
+
+var person = {
+    name: "Nicholas",
+    age: 29,
+    job: "Software Engineer",
+
+    sayName: function() {
+        alert(this.name);
+    }
+};
+
+/*
+*   属性类型
+* */
